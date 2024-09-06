3ab77b62d3a788f2d8f4f994631c6375c1078be5
devil <saikartheek352@gmal.com>
2024-09-06 11:25:29 +0530

    first commit

---
+++ src/components/TopSeller.js
@@ -0,0 +1,15 @@
+import seller1 from "../img/seller1.jpg";
+import seller2 from "../img/seller2.jpg";
+import seller3 from "../img/seller3.jpg";
+import seller4 from "../img/seller4.jpg";
+import seller5 from "../img/seller5.jpg";
+
+const TopSeller = [
+  { id: 1, seller_name: "Siri Kakara", username: "@Siri", imgSrc: seller1 },
+  { id: 2, seller_name: "John Mark", username: "@John", imgSrc: seller2 },
+  { id: 3, seller_name: "Likitha Ravada", username: "@Likitha", imgSrc: seller3 },
+  { id: 4, seller_name: "Brock Edwards", username: "@Brock", imgSrc: seller4 },
+  { id: 5, seller_name: "Percy Grace", username: "@Grace", imgSrc: seller5 },
+];
+
+export default TopSeller;
